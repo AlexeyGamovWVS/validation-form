299191b73d73f1a95d29a33d2f08a376b03533a6
Alexey Gamov <lexa2009xxxx@yandex.ru>
2022-06-21 18:25:54 +0300

    Information

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//функция показа спан-элемента с ошибкой и стилизации поля с ошибкой
+//на вход получает: форму, конкретное поле ввода, текст сообщения ошибки из стандартной валидации
 const showInputError = (formElement, inputElement, errorMessage) => {
   const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
   inputElement.classList.add('form__input_type_error');
@@ -5,6 +7,7 @@ const showInputError = (formElement, inputElement, errorMessage) => {
   errorElement.classList.add('form__input-error_active');
 };
 
+//функция скрытия спан-элемента и стилизация поля в дефолтное состояние
 const hideInputError = (formElement, inputElement) => {
   const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
   inputElement.classList.remove('form__input_type_error');
@@ -12,6 +15,8 @@ const hideInputError = (formElement, inputElement) => {
   errorElement.textContent = '';
 };
 
+//функция проверки валидности поля — на вход получает форму и поле
+// проводит проверку валидации, активирует функции скрытия / показа ошибки
 const checkInputValidity = (formElement, inputElement) => {
   if (!inputElement.validity.valid) {
     showInputError(formElement, inputElement, inputElement.validationMessage);
@@ -20,15 +25,20 @@ const checkInputValidity = (formElement, inputElement) => {
   }
 };
 
+//функция проверки некоторого массива полей 
+// на наличие хоть одного не валидного
+// Если поле не валидно, колбэк вернёт true
+// Обход массива прекратится и вся функция
+// hasInvalidInput вернёт true
 const hasInvalidInput = (inputList) => {
   return inputList.some((inputElement) => {
     return !inputElement.validity.valid;
   })
-  // Если поле не валидно, колбэк вернёт true
-  // Обход массива прекратится и вся функция
-  // hasInvalidInput вернёт true
 };
 
+//функция переключения состояния кнопки в зависимости
+// от валидности. На вход — список полей и кнопка, относящаяся к ним
+// Если хоть 1 поле не валидно, класс кнопки неактивна
 const toggleButtonState = (inputList, buttonElement) => {
   if (hasInvalidInput(inputList)) {
     buttonElement.classList.add('button_inactive');
@@ -37,6 +47,14 @@ const toggleButtonState = (inputList, buttonElement) => {
   }
 };
 
+// функция простановки отслеживания событий по кнопкам и инпутам
+// на вход — некоторая форма
+// создаёт массив полей вошедшей формы
+// определяет кнопку вошедшей формы
+// делает кнопку изначально неактивной
+// для массива инпутов вешает на каждый инпут обработчик,
+// который при каждом внесении символа проверяет валидность поля, 
+// а также активирует/деактивирует кнопку
 const setEventListeners = (formElement) => {
   const inputList = Array.from(formElement.querySelectorAll('.form__input'));
   const buttonElement = formElement.querySelector('.form__submit');
@@ -50,6 +68,8 @@ const setEventListeners = (formElement) => {
   });
 };
 
+// функция простановки отслеживания событий по формам, а также вызов
+// валидатора полей для каждой формы
 const enableValidation = () => {
   const formList = Array.from(document.querySelectorAll('.form'));
   formList.forEach((formElement) => {
